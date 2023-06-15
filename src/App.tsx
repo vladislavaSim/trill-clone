a661bee40f2827f807d254dc5a7934f303e54776
vladislavaSim <simonova.vladislava@gmail.com>
2023-06-15 20:38:56 +0200

    simple to do list with deleting a task is done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
-import React from 'react';
-import TodoList from './components/TodoList';
+import React, { useState } from 'react';
+import TodoList, { Task } from './components/TodoList';
+
+
+let tasks1: Task[] = [
+  {id: 1, name: 'wash a cat', isDone: false},
+  {id: 2, name: 'eat a sandwich', isDone: false},
+  {id: 3, name: 'go for a walk', isDone: false},
+  {id: 4, name: 'learn TS', isDone: false}
+]
 
 function App() {
+
+  const [tasks, setTasks] = useState(tasks1)
+  
+  function deleteTask(task: Task) {
+    setTasks(tasks.filter(t => t.id !== task.id))
+  }
+
   return (
     <div className="App">
-      <TodoList />
+      <TodoList title={'My tasks'} tasks={tasks} deleteTask={deleteTask}/>
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,7 +1,40 @@
 import React from 'react'
 
-export default function TodoList() {
+export type Task = {
+    id: number,
+    name: string,
+    isDone: boolean
+}
+
+type PropsType = {
+    title: string | undefined,
+    tasks: Task[],
+    deleteTask: (task: Task) => void
+}
+export default function TodoList(props: PropsType) {
   return (
-    <div>TodoList</div>
+    <>
+        <h3>{props.title}</h3>
+        <div>
+            <input type="text" />
+            <button>add</button>
+        </div>
+        <div>
+            <ul>
+                {props.tasks.map(task => {
+                    return <li>
+                        <span>{task.name}</span>
+                        {/* <input type="checkbox" checked={task.isDone}/> */}
+                        <button onClick={() => props.deleteTask(task)}>x</button>
+                    </li>
+                })}
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Completed</button>
+                <button>Active</button>
+            </div>
+        </div>
+    </>
   )
 }
